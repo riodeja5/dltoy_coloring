32e85c8937fdde899d9c3faa54092b1b70ad363d
shota <riodeja.5.shota@gmail.com>
2019-05-03 12:44:25 +0900

    use VGG16

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
+import numpy as np
 from keras.applications import VGG16
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+from keras.preprocessing.image import load_img, img_to_array
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
@@ -43,6 +46,22 @@ def dltoy(request):
     if request.method == "POST":
         # TOOD proc dl
         post = "at if"
+        print('!#!# Debug Start.')
+        model = VGG16()
+        img_dog = load_img('img/dog.jpg', target_size=(224, 224))
+        img_cat = load_img('img/cat.jpg', target_size=(224, 224))
+        arr_dog = img_to_array(img_dog)
+        arr_cat = img_to_array(img_cat)
+        arr_dog = preprocess_input(arr_dog)
+        arr_cat = preprocess_input(arr_cat)
+        arr_input = np.stack([arr_dog, arr_cat])
+
+        # prediction
+        probs = model.predict(arr_input)
+        results = decode_predictions(probs)
+        print(results[0])
+        print('!#!# Debug End.')
+        post = results[0]
     else:
         #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
         post = "at else"
