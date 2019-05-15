d3f649b04a1401552f61db63a679cdd8478bb1ae
shota <riodeja.5.shota@gmail.com>
2019-05-16 06:57:47 +0900

    delete unnecessary files and methods

--- blog/urls.py
+++ blog/urls.py
@@ -6,11 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.upload_file, name='upload'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('dltoy', views.dltoy, name='dltoy'),
-    path('upload', views.upload_file, name='upload'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,13 @@ from keras.models import load_model
 from keras.applications import VGG16
 from keras.applications.vgg16 import preprocess_input, decode_predictions
 from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.views import generic
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import PostForm
-#from .forms import UploadForm
 from .forms import UploadFileForm
 
 # Imaginary function to handle an uploaded file.
@@ -20,66 +20,6 @@ from .forms import UploadFileForm
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 tmp_img = os.path.join(THIS_FOLDER, 'tmp/tmp_img')
 graph = tf.get_default_graph()
-
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
-
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
-
-def dltoy(request):
-    if request.method == "POST":
-        # TOOD proc dl
-        post = "at if"
-        print('!#!# Debug Start.')
-        model = VGG16()
-        img_dog = load_img('img/dog.jpg', target_size=(224, 224))
-        img_cat = load_img('img/cat.jpg', target_size=(224, 224))
-        arr_dog = img_to_array(img_dog)
-        arr_cat = img_to_array(img_cat)
-        arr_dog = preprocess_input(arr_dog)
-        arr_cat = preprocess_input(arr_cat)
-        arr_input = np.stack([arr_dog, arr_cat])
-
-        # prediction
-        probs = model.predict(arr_input)
-        results = decode_predictions(probs)
-        print(results[0])
-        print('!#!# Debug End.')
-        post = results[0]
-    else:
-        #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-        post = "at else"
-    return render(request, 'blog/dltoy.html', {'post': post})
 
 def upload_file(request):
 
