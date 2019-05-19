a96da84fb16e92dca4ba54b8e194265bb4826c52
shota <riodeja.5.shota@gmail.com>
2019-05-19 14:42:39 +0900

    display colored image

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 import os
+import glob
+import math
+import cv2
 import numpy as np
 import tensorflow as tf
 from keras.models import load_model 
 from keras.applications import VGG16
 from keras.applications.vgg16 import preprocess_input, decode_predictions
 from keras.preprocessing.image import load_img, img_to_array
-from keras.models import load_model
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.views import generic
@@ -19,6 +21,7 @@ from .forms import UploadFileForm
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 tmp_img = os.path.join(THIS_FOLDER, 'tmp/tmp_img')
+img_size = 224
 graph = tf.get_default_graph()
 
 def upload_file(request):
@@ -31,7 +34,7 @@ def upload_file(request):
         if form.is_valid():
             print('Debug upload if if')
             handle_uploaded_file(request.FILES['file'])
-            #post = predict()
+            post = predict()
             post = None
             predicted = True
 
@@ -52,24 +55,74 @@ def handle_uploaded_file(f):
             destination.write(chunk)
 
 def predict():
+    batch_size = 30
     post = None
 
     global graph
     with graph.as_default():
-        model = VGG16()
+        autoencoder = load_model('blog/static/autoencoder.h5')
 
-        #img = load_img('blog/tmp/tmp_img', target_size=(224, 224))
-        img = load_img(tmp_img, target_size=(224, 224))
-        arr_data = img_to_array(img)
-        arr_data = preprocess_input(arr_data)
-        arr_input = np.stack([arr_data])
+        input_lists = glob.glob(tmp_img)
+        input_lab = get_lab_from_data_list(input_lists)
+        input_steps = math.ceil(len(input_lists)/batch_size)
+        input_gen = generator_with_preprocessing(input_lists, batch_size)
 
-        # prediction
-        probs = model.predict(arr_input)
-        results = decode_predictions(probs)
-        print(results[0])
-        print('!#!# Debug End.')
-        post = results[0]
+        input_pred = autoencoder.predict_generator(input_gen, steps=input_steps, verbose=0)
+
+        x_input = []
+        y_input = []
+        for i, (l, ab) in enumerate(generator_with_preprocessing(input_lists, batch_size)):
+            x_input.append(l)
+            y_input.append(ab)
+            if i == (input_steps - 1):
+                break
+        x_input = np.vstack(x_input)
+        y_input = np.vstack(y_input)
+
+        # LAB->RGB
+        input_preds_lab = np.concatenate((x_input, input_pred), 3).astype(np.uint8)
+
+        input_preds_rgb = []
+        for i in range(input_preds_lab.shape[0]):
+            preds_rgb = lab2rgb(input_preds_lab[i, :, :, :])
+            input_preds_rgb.append(preds_rgb)
+        input_preds_rgb = np.stack(input_preds_rgb)
+
+        # save result
+        cv2.imwrite('media/images/output.jpg', input_preds_rgb[0])
 
         return post
+
+def rgb2lab(rgb):
+    assert rgb.dtype == 'uint8'
+    return cv2.cvtColor(rgb, cv2.COLOR_RGB2LAB)
+
+def lab2rgb(lab):
+    assert lab.dtype == 'uint8'
+    return cv2.cvtColor(lab, cv2.COLOR_LAB2RGB)
+
+def get_lab_from_data_list(data_list):
+    x_lab = []
+    for f in data_list:
+        rgb = img_to_array(
+            load_img(
+                f,
+                target_size=(img_size, img_size)
+            )
+        ).astype(np.uint8)
+        lab = rgb2lab(rgb)
+        x_lab.append(lab)
+    return np.stack(x_lab)
+
+def generator_with_preprocessing(data_list, batch_size, shuffle=False):
+    while True:
+       if shuffle:
+           np.random.shuffle(data_list)
+       for i in range(0, len(data_list), batch_size):
+           batch_list = data_list[i:i + batch_size]
+           batch_lab = get_lab_from_data_list(batch_list)
+           batch_l = batch_lab[:, :, :, 0:1]
+           batch_ab = batch_lab[:, :, :, 1:]
+           yield(batch_l, batch_ab)
+
 
